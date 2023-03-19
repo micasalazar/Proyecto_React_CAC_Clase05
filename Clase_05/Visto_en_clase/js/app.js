6963b39e34df1481ed0287d8970092c5a1714a12
micasalazar <micaela.salazar1586@gmail.com>
2023-03-19 13:33:52 -0300

    Ejercicio Clse 05

--- Clase_05/Visto_en_clase/js/app.js
+++ Clase_05/Visto_en_clase/js/app.js
@@ -31,5 +31,59 @@ function RenderButton(text){
     let myButton =  new Button("app",text)
     myButton.render();
 }
+
+
 // Por cada uno de los paremetros que tengo dentro de mySuperArrayToString-->llamame a esta funcion RenderButton()
 mySuperArrayToString.forEach(element => RenderButton(element));
+
+
+function MiSuperFuncion(){
+
+}
+MiSuperFuncion();//Invocamos...
+//Puedo asignar una constante a una funcion que NO tiene nombre
+
+const MiSuperFuncionDefinida= function(){//--->funcion anonima
+
+}
+
+MiSuperFuncionDefinida();
+
+//Definir funciones asignando otras..
+const MiSegundaSuperFuncion = MiSuperFuncion;
+
+// Cualquier cosa que gusrademos en una variable puede ser una funcion o puede ser un objeto
+
+///Otro tipo de funcion anonima-->conocida como ARROW FUNCTION/FUNCION FLECHA
+const MiSuperFuncionSinNombre = () => {
+
+}
+
+
+////////////////
+// Resolvemos el ejercicio del dia
+let inputArray = ["Username", "Surname", "Password","email"];
+
+function RenderInput(type){
+    let input_type = "none"
+
+    switch(type){
+        case "Password":
+            input_type = "password"
+            break;
+            case "E-mail":
+                input_type = "email"
+                break;
+     default:
+        input_type="text"
+        break;
+    }
+
+    let myInput =  new Input("app",input_type)
+    myInput.render();
+
+}
+
+inputArray.forEach(function(e){
+    RenderInput(e);
+})
